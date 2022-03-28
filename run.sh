829bd3e058d3f5db05e02a8ebc88292069192928
jahansabit <ssarwarjahan1337@gmail.com>
2022-03-28 23:34:50 +0600

    probable fix for bash command

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 while true
 do
-	xfce4-terminal -e 'run_scraper.sh' &
+	xfce4-terminal -e '/root/tg_bot/run_scraper.sh' &
 	python3 tg_bot.py | tee -a ../logs/bot.log
 	bash crash_sender.sh
 	sleep 2
