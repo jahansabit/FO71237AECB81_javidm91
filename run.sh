5f75b49638200d36edc668280e2db44ccf9c509e
jahansabit <ssarwarjahan1337@gmail.com>
2022-03-28 23:47:02 +0600

    changed bash files

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
+xfce4-terminal -e '/root/tg_bot/run_scraper.sh' &
+
 while true
 do
-	xfce4-terminal -e '/root/tg_bot/run_scraper.sh' &
 	python3 tg_bot.py | tee -a ../logs/bot.log
 	bash crash_sender.sh
 	sleep 2
