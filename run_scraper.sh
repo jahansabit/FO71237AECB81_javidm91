40ddcfca95a9727cc3dfb916c2cbe911cbf6da3f
jahansabit <ssarwarjahan1337@gmail.com>
2022-03-28 21:19:16 +0600

    changed commands for 2 files

--- run_scraper.sh
+++ run_scraper.sh
@@ -3,8 +3,7 @@ cd "$(dirname "$0")"
 
 while true
 do
-	xfce4-terminal -e 'run_scraper.sh' &
-	python3 tg_bot.py | tee -a ../logs/bot.log
+	python3 scraper_bot.py | tee -a ../logs/bot.log
 	bash crash_sender.sh
 	sleep 2
 done
