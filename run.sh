90876f9858ce72c78b96eb93393f3115ad4e6743
ms-jahan <ssarwarjahan@gmail.com>
2022-04-23 16:02:47 +0600

    updated bash scripts

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ xfce4-terminal -e '/root/tg_bot/run_scraper.sh' &
 
 while true
 do
-	python3 tg_bot.py | tee -a ../logs/bot.log
+	# python3 tg_bot.py | tee -a ../logs/bot.log
+	python3 tg_bot.py
 	bash crash_sender.sh
 	sleep 2
 done
--- run_scraper.sh
+++ run_scraper.sh
@@ -3,7 +3,8 @@ cd "$(dirname "$0")"
 
 while true
 do
-	python3 scraper_bot.py | tee -a ../logs/bot.log
+	# python3 scraper_bot.py | tee -a ../logs/bot.log
+	python3 scraper_bot.py
 	bash crash_sender.sh
 	sleep 2
 done
